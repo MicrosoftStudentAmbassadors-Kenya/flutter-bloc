9ae012bb398b6f0cafce5e7affb5628ad0750636
Toxic254 <cyrus_g@outlook.com>
2022-01-26 23:51:41 +0300

    Refactored our increament fn to pass wasIncreament to true and false for decreament.

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -5,7 +5,9 @@ part 'counter_state.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0));
 
-  void increament() => emit(CounterState(counterValue: state.counterValue + 1));
+  void increament() => emit(
+      CounterState(counterValue: state.counterValue + 1, wasIncreament: true));
 
-  void decreament() => emit(CounterState(counterValue: state.counterValue - 1));
+  void decreament() => emit(
+      CounterState(counterValue: state.counterValue - 1, wasIncreament: false));
 }
