62b5eb9c972c60bc175aa11440ae08862a0cc845
Toxic254 <cyrus_g@outlook.com>
2022-01-27 20:35:51 +0300

    Refactored our main app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:blocapp/cubit/counter_cubit.dart';
+import 'package:blocapp/logic/cubit/counter_cubit.dart';
+import 'package:blocapp/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,80 +19,9 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
         ),
-        home: const MyHomePage(title: 'Flutter Bloc App'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncreament == true) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Increamented'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                } else if (state.wasIncreament == false) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Decreamented'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                }
-              },
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decreament();
-                  },
-                  tooltip: 'Decreament',
-                  child: const Icon(Icons.exposure_minus_1),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increament();
-                  },
-                  tooltip: 'Increament',
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
+        home: const HomeScreen(
+          title: 'Flutter Bloc App',
+          color: Colors.yellow,
         ),
       ),
     );
