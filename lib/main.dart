a87a357179e65fff38215921dbd0be9fbbf85dd3
Toxic254 <cyrus_g@outlook.com>
2022-01-27 21:42:12 +0300

    Successfully implemented named routing in our app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,62 @@
 import 'package:blocapp/logic/cubit/counter_cubit.dart';
 import 'package:blocapp/presentation/screens/home_screen.dart';
+import 'package:blocapp/presentation/screens/second_screen.dart';
+import 'package:blocapp/presentation/screens/third_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  final CounterCubit _counterCubit = CounterCubit();
+
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final CounterCubit _counterCubit = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter BLoC App',
-        theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-        ),
-        home: const HomeScreen(
-          title: 'Flutter Bloc App',
-          color: Colors.yellow,
-        ),
+    return MaterialApp(
+      title: 'Flutter BLoC App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
+      routes: {
+        '/': (context) => BlocProvider.value(
+              value: _counterCubit,
+              child: const HomeScreen(
+                title: 'BLoC HomePage',
+                color: Colors.purpleAccent,
+              ),
+            ),
+        '/second': (context) => BlocProvider.value(
+              value: _counterCubit,
+              child: const SecondScreen(
+                title: 'Second Page',
+                color: Colors.red,
+              ),
+            ),
+        '/third': (context) => BlocProvider.value(
+              value: _counterCubit,
+              child: const ThirdScreen(
+                title: 'Third Page',
+                color: Colors.lightGreenAccent,
+              ),
+            ),
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    _counterCubit.close();
+    super.dispose();
   }
 }
