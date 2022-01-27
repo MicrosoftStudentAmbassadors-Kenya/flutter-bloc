8eefce8389d8fa25dfa42555dbc498c54c17a147
Toxic254 <cyrus_g@outlook.com>
2022-01-27 20:36:58 +0300

    Created our second screen to implement anonymous routing.

---
+++ lib/presentation/screens/second_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:blocapp/logic/cubit/counter_cubit.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key, required this.title, required this.color})
+      : super(key: key);
+
+  final String title;
+  final Color color;
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncreament == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Increamented'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                } else if (state.wasIncreament == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Decreamented'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  heroTag: const Text('Decreament'),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decreament();
+                  },
+                  tooltip: 'Decreament',
+                  child: const Icon(Icons.exposure_minus_1),
+                ),
+                FloatingActionButton(
+                  heroTag: const Text('Increament'),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increament();
+                  },
+                  tooltip: 'Increament',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
