658227fe5c7a6d551a57e739d13b3181a249d757
Toxic254 <cyrus_g@outlook.com>
2022-01-28 12:23:12 +0300

    Migrated from named routing to generated routing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blocapp/logic/cubit/counter_cubit.dart';
+import 'package:blocapp/presentation/router/app_router.dart';
 import 'package:blocapp/presentation/screens/home_screen.dart';
 import 'package:blocapp/presentation/screens/second_screen.dart';
 import 'package:blocapp/presentation/screens/third_screen.dart';
@@ -6,21 +7,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  final CounterCubit _counterCubit = CounterCubit();
-
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final CounterCubit _counterCubit = CounterCubit();
-
+  final AppRouter _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,35 +27,36 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      routes: {
-        '/': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: const HomeScreen(
-                title: 'BLoC HomePage',
-                color: Colors.purpleAccent,
-              ),
-            ),
-        '/second': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: const SecondScreen(
-                title: 'Second Page',
-                color: Colors.red,
-              ),
-            ),
-        '/third': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: const ThirdScreen(
-                title: 'Third Page',
-                color: Colors.lightGreenAccent,
-              ),
-            ),
-      },
+      onGenerateRoute: _appRouter.onGenerateRoute,
+      // routes: {
+      //   '/': (context) => BlocProvider.value(
+      //         value: _counterCubit,
+      //         child: const HomeScreen(
+      //           title: 'BLoC HomePage',
+      //           color: Colors.purpleAccent,
+      //         ),
+      //       ),
+      //   '/second': (context) => BlocProvider.value(
+      //         value: _counterCubit,
+      //         child: const SecondScreen(
+      //           title: 'Second Screen',
+      //           color: Colors.red,
+      //         ),
+      //       ),
+      //   '/third': (context) => BlocProvider.value(
+      //         value: _counterCubit,
+      //         child: const ThirdScreen(
+      //           title: 'Third Screen',
+      //           color: Colors.lightGreenAccent,
+      //         ),
+      //       ),
+      // },
     );
   }
 
   @override
   void dispose() {
-    _counterCubit.close();
+    _appRouter.dispose();
     super.dispose();
   }
 }
