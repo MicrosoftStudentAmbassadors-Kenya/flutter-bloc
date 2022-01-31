bdbfed32c64400c1e9904bf23fdb1613556228e5
Toxic254 <cyrus_g@outlook.com>
2022-01-31 12:39:49 +0300

    Reafctored, added connectivity to the composer, provided cubits globally.

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,45 @@
 import 'package:blocapp/logic/cubit/counter_cubit.dart';
+import 'package:blocapp/logic/cubit/internet_cubit.dart';
 import 'package:blocapp/presentation/router/app_router.dart';
-import 'package:blocapp/presentation/screens/home_screen.dart';
-import 'package:blocapp/presentation/screens/second_screen.dart';
-import 'package:blocapp/presentation/screens/third_screen.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp(
+    appRouter: AppRouter(),
+    connectivity: Connectivity(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+  final Connectivity connectivity;
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+  const MyApp({
+    Key? key,
+    required this.appRouter,
+    required this.connectivity,
+  }) : super(key: key);
 
-class _MyAppState extends State<MyApp> {
-  final AppRouter _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter BLoC App',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<InternetCubit>(
+            create: (internetCubitContext) =>
+                InternetCubit(connectivity: connectivity)),
+        BlocProvider<CounterCubit>(
+            create: (counterCubitContext) => CounterCubit())
+      ],
+      child: MaterialApp(
+        title: 'Flutter BLoC App',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
-      // routes: {
-      //   '/': (context) => BlocProvider.value(
-      //         value: _counterCubit,
-      //         child: const HomeScreen(
-      //           title: 'BLoC HomePage',
-      //           color: Colors.purpleAccent,
-      //         ),
-      //       ),
-      //   '/second': (context) => BlocProvider.value(
-      //         value: _counterCubit,
-      //         child: const SecondScreen(
-      //           title: 'Second Screen',
-      //           color: Colors.red,
-      //         ),
-      //       ),
-      //   '/third': (context) => BlocProvider.value(
-      //         value: _counterCubit,
-      //         child: const ThirdScreen(
-      //           title: 'Third Screen',
-      //           color: Colors.lightGreenAccent,
-      //         ),
-      //       ),
-      // },
     );
-  }
-
-  @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
